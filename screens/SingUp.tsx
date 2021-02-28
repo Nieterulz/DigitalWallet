4ab191c143256a1ecbcda647ff94ba51e7d03f3d
Antonio José Sánchez Muñoz <ajsamu@hotmail.com>
2021-02-28 16:14:14 +0100

    Initial commit

---
+++ screens/SingUp.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SingUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+        </View>
+    );
+};
+
+export default SingUp;
+
+const styles = StyleSheet.create({});
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Scan from "./Scan";
+import SingUp from "./SingUp";
+
+export { Home, Scan, SingUp };
